86d228bcee5e64eeca6f017b1586a66d629c1b1f
lemongrasshat <lemongrasshat@gmail.com>
2022-03-10 20:36:50 +0530

    DOM insertion and devent

---
+++ js_dom_3_events.js
@@ -0,0 +1,32 @@
+
+// let buttonInsert=(event)=>{
+//     document.getElementById('myheader').style.color='green';
+//     console.log(event.target);// selected button
+// }
+// let button =document.getElementById('insert');
+// button.addEventListener('click',buttonInsert); //event
+
+// 
+// event.clientX --> window x location from browser/window
+// event.clienty --> window y location from browser/window
+// event.offsetX --> actual element dimensions x location from browser/window
+// event.offsety --> actual element dimensions location from browser/window
+
+// let runEvent=(str)=>{
+//     return (e)=>{
+//         document.getElementById('myheader').innerText=str+`${e.type}`;
+//     }
+//  }
+// passing str to func
+// let button =document.getElementById('insert');
+// //button.addEventListener('dblclick',runEvent);
+// // button.addEventListener('mouseup',runEvent);
+// // button.addEventListener('mousedown',runEvent);
+//  button.addEventListener('click',runEvent("MOUSER ENTER"));
+//  button.addEventListener('dblclick',runEvent("MOSSUE LEAVE"));
+// Similiary more events are input change submit etc
+// let keyEvent=(event)=>{
+//     ocument.getElementById('myheader').innerText=str+`${e.type}`;
+// }
+//  let button =document.getElementById('insert');
+//  button.addEventListener('keydown',keyEvent);
